077cbafc22ce74c3f88fde88dca001be80b9973d
J. nathan Matias <natematias@gmail.com>
2011-09-04 17:03:59 -0400

    fetch last audited action for a shared thing

--- app/models/shared_thing.rb
+++ app/models/shared_thing.rb
@@ -1,3 +1,7 @@
 class SharedThing < ActiveRecord::Base
-  has_many :audited_actions
+  has_many :audited_actions, :order => 'created_at DESC'
+  def last_action()
+    return nil if self.audited_actions.count < 1
+    self.audited_actions[-1]
+  end
 end
--- test/unit/shared_thing_test.rb
+++ test/unit/shared_thing_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class SharedThingTest < ActiveSupport::TestCase
+  test "find the latest status" do
+    assert_equal "Items displaced", shared_things(:washer).last_action.message
+    assert_equal "Load removed", shared_things(:dryer_one).last_action.message
+    assert_nil shared_things(:dryer_two).last_action
+  end
 end
